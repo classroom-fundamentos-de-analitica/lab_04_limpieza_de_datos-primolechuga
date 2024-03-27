168f5145a73f44b11780d627484c3a08d23cb5da
primolechuga <juanestebaca123@gmail.com>
2024-03-27 12:39:45 -0500

    FC

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,20 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
+import re
 
 
 def clean_data():
-
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.DataFrame(df)   
+    df = df.applymap(lambda s:s.lower() if type(s) == str else s)
+    df[['barrio', 'idea_negocio', 'línea_credito']] = df[['barrio', 'idea_negocio', 'línea_credito']].replace(['_', '-'], ' ', regex=True)
+    df = df.dropna()
+    df = df.drop(df.columns[0], axis=1)
+    patron =r'\d{4}'
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: '/'.join(x.split('/')[::-1]) if re.match(patron, x) else x)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio']).dt.strftime('%d/%m/%Y')
+    df['barrio'] = df['barrio'].str.replace('bel¿n', 'belen').str.replace('antonio nari¿o', 'antonio nariño').str.replace('san jose de la cima no. ', 'san jose de la cima no.').str.replace('el salado ', 'el salado')
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '').str.replace('.00', '').str.replace(',', '').astype(int)
+    df = df.drop_duplicates()
     return df
